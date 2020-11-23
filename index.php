fb3314c5e7b9e6f5ff9e675cf75ce5e91f0959b6
Opilane TTHK <opilane@tthk.ee>
2020-11-23 09:13:35 +0200

    Ready seach form and JSON file

--- index.php
+++ index.php
@@ -145,3 +145,43 @@ $xml = simplexml_load_file("andmed.xml");
     }
     ?>
 </table>
+<h2>Поиск по имени заказчика:</h2>
+<form action="?" method="post">
+    <input type="text" name="search" id="search" value="" placeholder="Tellija nimi">
+    <button>Поиск</button>
+</form>
+<br/>
+<table border="1">
+    <tr>
+        <th>Toodenimi</th>
+        <th>Kogus</th>
+        <th>Hind</th>
+        <th>Tellija Nimi</th>
+        <th>Tellija Perenimi</th>
+        <th>Tellija Isikukood</th>
+        <th>Tellija Address</th>
+        <th>Kuupäev</th>
+    </tr>
+<?php
+$query = $_POST['search'];
+foreach ($xml->children() as $arve)
+{
+    $tellija = $arve -> tellija;
+    if($tellija->nimi == $query){
+        echo "<tr>
+            <td>".$arve->toodenimi."</td>
+            <td>$arve->kogus</td>
+            <td>$arve->hind</td>
+            <td>$tellija->nimi</td>
+            <td>$tellija->perenimi</td>
+            <td>$tellija->isikukood</td>
+            <td>$tellija->address</td>
+            <td>$arve->kuupaev</td>
+            </tr>";
+
+    }
+
+}
+?>
+</table>
+<h1><a href="andmed.json">JSON файл</a></h1>
